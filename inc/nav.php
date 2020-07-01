0ded6b91e7dac1a2d5251edbc63bbf49baaf57bb
msquidward17 <67696489+msquidward17@users.noreply.github.com>
2020-07-01 13:07:52 -0400

    Changed file name to fix menu highlighter problem

--- inc/nav.php
+++ inc/nav.php
@@ -1,7 +1,7 @@
 <nav class="menu">
 	<ul>
 		<li><a href="abstart.php">The Spitfire Grill</a></li>
-		<li><a href="start.php">Dead Poets Society</a></li>
+		<li><a href="mkstart.php">Dead Poets Society</a></li>
 		<li><a href="hystart.php">Hollow Knight</a></li>
 	</ul>
 </nav>
